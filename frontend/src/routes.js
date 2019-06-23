d66d00402722581914ef3d9171ad582587864f48
Alex Matos da Silva <alex.matos@ufms.br>
2019-06-22 23:57:49 -0400

    frontend feed

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Feed from './pages/Feed'
 import New from './pages/New'
